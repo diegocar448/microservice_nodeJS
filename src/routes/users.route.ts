a6d91f35898779bd015869379cd16b9d6cf0dc29
Diego Cardoso <diegocar448@hotmail.com>
2022-01-05 15:23:21 -0300

    Aqui criamos a estrutura de rota enviando e pegando o parâmetro rota pode setar tbm uma tipagem para o parâmetro em ( users.route.ts )

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,10 +1,10 @@
 import { Router, Request, Response,  NextFunction, response } from 'express';
 
-// get/users
+//configuração de rota
 const usersRoute = Router();
 
 
-//configuração de rota
+// get/users
 usersRoute.get('/users', (req:Request, res:Response, next:NextFunction) => {
     const users = [{ userName: 'Diego' }];
     res.status(200).send( users );
@@ -12,9 +12,16 @@ usersRoute.get('/users', (req:Request, res:Response, next:NextFunction) => {
 
 export default usersRoute;
 
-
-
 // get /:uuid
+// podemos tbm tipar o parâmetro em Request com o Request<{ uuid:string }>
+usersRoute.get('/users/:uuid', (req:Request<{ uuid: string }>, res:Response, next:NextFunction) => {
+
+    //aqui pegar o valor passado na url
+    const uuid = req.params.uuid;    
+    res.status(200).send({ uuid });
+})
+
+
 // post /users
 // put /users/:uuid
 // delete /users/:uuid
