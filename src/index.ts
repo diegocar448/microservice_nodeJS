f2615b193b094ca7dd8ccea6c783a1517147237b
Diego Cardoso <diegocar448@hotmail.com>
2022-01-05 11:08:58 -0300

    Estrutura inicial do microserviço com NodeJS

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Executei!")
+import express, {Request, Response, NextFunction} from 'express';
+
+const app = express();
+
+
+/* parâmetro com a tipagem e vai fazer a requisição*/
+app.get('/status', (req: Request, res: Response, next:NextFunction) => {
+    res.status(200).send({foo: 'bar'});
+});
+
+/* Aqui definimos em qual porta será executada o nosso app */
+app.listen(3000, () =>{
+    console.log('Aplicação executando na porta 3000!');
+});
